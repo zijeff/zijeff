b8c929ed6dcae3ccc57c8c50591d8122f19fa42a
zijeff <3875125974@qq.com>
2025-05-16 19:46:15 +0800

    Add files via upload

--- CTF密码妙妙屋/XSCTF联合招新赛/babyfibo/exp.py
+++ CTF密码妙妙屋/XSCTF联合招新赛/babyfibo/exp.py
@@ -1,11 +1,20 @@
-# by zijeff
-# 简单的异或加密
-# 不过要求的s是通过斐波那契数列得到的
-# 也许这道题考察的是编程能力?
-# 小声逼逼：sagemath有现成的斐波那契数列求解函数，直接用就可以求任一项的值了
-
-import libnum
-s=43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875
-c=43104378128345818181217961835377190975779804452524643191544804229536124095677294719566215359919831933542699064892141754715180028183150724886016542388159082125737677224886528142312511700711365919689756090950704
-m=c^s
-print(libnum.n2s(m))
+import base64
+import random
+c = b'8OcTbAfL6/kOMQnC9v8SNmmSzvQMeGTT8vANM1T+7vIce2fo0fc2RnScrNxTSmeSyuMjMF//w8BWaXX91dsGcnvmreg0NQTw96ceVVXj3sQ3Znn51OU1S0bOyaMtNHTj36AcWFqewN4zRUXD6agGbAPE+tQtd3XG0doAa1Ll9fhcQ1zk0McTM1bv8PIQOAnn3vQ3UgLD3PsONXLs4KkXMnjTyMEQOFn/0uYVUwOY1PsleEHCyNopRVDr+Kc0e2PH9v0XNXfprfIPU3nw7KYTNX/G7twLSkHoyaUlQHXi3v02UHmdy/4iNgme3Pc8bgPp+tYWV1+YzPkXYkXM4ulUc27DrM4SNUPT2fQlckj1qP4Fal+YoPYJMlyZ8qhXfF3Y0tUDdUXl3vg0dFTi++VVOFfH/dgMS1ru9N8WU0HF9cUCTgPe+qVdSn/u7Mkda0GTw/QDcWPZ9KYGN2jSzfk0OVrMzt0yRHD64KMrUgPF2sFWcmP56KZSTAD61PUGeXrd49MgU1bL8OsVNWj91vIsalXwqf0qaWbwzv0lWETA4eElS3L99cYmU1nv9dRQTWbDyclScQTN6NIhV2j//+ZWbH7Z68kwM3Dy4dcUc1PQy8kRTl/4zcU9WGWfoakOMXuf69MXZQTEz+kJT1Dar8UN'
+def f(x):
+    random.seed(x)
+    return random.getrandbits(8)
+encrypted_bytes = base64.b64decode(c)
+encrypted_ints = list(encrypted_bytes)
+for r in range(2,250):
+    for key in range(1,256):
+        flag = []
+        for j in range(len(encrypted_ints)):
+            key = f(key)
+            tmp = (key * r) % 251
+            temp = encrypted_ints[j] ^ tmp
+            flag.append(temp)
+        flag = bytes(flag)
+        if b'hsctf' in flag:
+            print(flag)
+            break
