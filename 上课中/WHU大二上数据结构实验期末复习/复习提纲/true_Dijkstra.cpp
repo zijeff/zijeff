48775d01f5da9a5e098d34fb7d916d1c770081c0
zijeff <3875125974@qq.com>
2025-12-20 13:58:24 +0800

    Create true_Dijkstra.cpp

---
+++ 上课中/WHU大二上数据结构实验期末复习/复习提纲/true_Dijkstra.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+using edge = pair<int, int>; // 邻接表中的边，[node, weight]节点编号在前，权重在后。
+using qe = pair<int, int>;   // QueueEleme——优先队列中的元素，[node, dist[node]]节点编号与给定节点到当前节点的最短距离
+struct cmp {
+    bool operator()(const edge &a, const edge &b) const { return a.second > b.second; }
+};
+vector<int> Dijkstra(vector<vector<edge>> &graph, int begin) {
+    vector<int> dist(graph.size(), INT_MAX);
+    vector<bool> visited(graph.size(), false);
+    priority_queue<edge, vector<edge>, cmp> s;
+    dist[begin] = 0;
+    qe current = {begin, dist[begin]};
+    s.push(current);
+    while (!s.empty()) {
+        current = s.top();
+        s.pop();
+        if (visited[current.first] == true)
+            continue;
+        visited[current.first] = true;
+        for (auto &e : graph[current.first]) {
+            if (dist[current.first] + e.second < dist[e.first]) {
+                dist[e.first] = dist[current.first] + e.second;
+                s.push({e.first, dist[e.first]});
+            }
+        }
+    }
+    return dist;
+}
+int main() {
+    vector<vector<pair<int, int>>> graph(7);
+
+    // 从 0 出发
+    graph[0].push_back({1, 1});
+    graph[0].push_back({2, 1});
+    graph[0].push_back({3, 10});
+
+    // 从 1 出发
+    graph[1].push_back({2, 1});
+    graph[1].push_back({6, 100});
+
+    // 从 2 出发
+    graph[2].push_back({3, 1});
+
+    // 从 3 出发
+    graph[3].push_back({4, 1});
+
+    // 从 4 出发
+    graph[4].push_back({5, 1});
+
+    // 从 5 出发
+    graph[5].push_back({6, 1});
+
+    vector<int> dist = Dijkstra(graph, 0);
+
+    for (int i = 0; i < dist.size(); i++) {
+        cout << "dist[0 -> " << i << "] = " << dist[i] << endl;
+    }
+}
