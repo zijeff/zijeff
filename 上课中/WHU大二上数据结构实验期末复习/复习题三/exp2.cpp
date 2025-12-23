924481f6daafa6e8d2ba647498c114c25288d6b6
zijeff <3875125974@qq.com>
2025-12-24 01:05:57 +0800

    Update exp2.cpp

--- 上课中/WHU大二上数据结构实验期末复习/复习题三/exp2.cpp
+++ 上课中/WHU大二上数据结构实验期末复习/复习题三/exp2.cpp
@@ -1,1 +1,42 @@
-dwdadadw
+#include <bits/stdc++.h>
+using namespace std;
+void output(vector<int> &ans) {
+    for (auto num : ans) {
+        cout << num << ' ';
+    }
+    cout << endl;
+}
+struct Frame {
+    vector<int> ans;
+    int n;
+    int i;
+};
+int main() {
+    int n = 0;
+    cin >> n;
+    vector<int> ans(2 * n, 0);
+    stack<Frame> s;
+    Frame begin = {ans, n, 0};
+    s.push(begin);
+    while (!s.empty()) {
+        Frame &f = s.top();
+        if (f.n == 0) {
+            output(f.ans);
+            s.pop();
+            continue;
+        }
+        if (f.i >= f.ans.size() - f.n - 1) {
+            s.pop();
+            continue;
+        }
+        int i = f.i;
+        f.i++;
+        if (f.ans[i] == 0 && f.ans[i + f.n + 1] == 0) {
+            vector<int> next = f.ans;
+            next[i] = f.n;
+            next[i + f.n + 1] = f.n;
+            s.push({next, f.n - 1, 0});
+        }
+    }
+    return 0;
+}
