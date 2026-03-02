e2da222a683108566f2231195e07f7895bb641ce
zijeff <3875125974@qq.com>
2026-03-02 15:47:51 +0800

    Add files via upload

---
+++ CTF密码妙妙屋/ISCTF2025/沉迷数学的小蓝鲨/椭圆曲线.py
@@ -0,0 +1,7 @@
+y² = x³ + 3x + 27 (mod p)
+
+Q(0xa61ae2f42348f8b84e4b8271ee8ce3f19d7760330ef6a5f6ec992430dccdc167, 0x8a3ceb15b94ee7c6ce435147f31ca8028d1dd07a986711966980f7de20490080)
+
+k= ?
+
+最终flag请将解出k值的16进制转换为32位md5以ISCTF{}包裹提交
